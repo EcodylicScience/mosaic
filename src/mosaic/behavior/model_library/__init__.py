d51e7269183664e4a46ce8a1c4583437e1ff9e3b
jacobdavidson <jddavid@umich.edu>
2026-06-09 08:36:43 +0200

    remove depreciated model files

--- src/mosaic/behavior/model_library/__init__.py
+++ src/mosaic/behavior/model_library/__init__.py
@@ -1,28 +1,12 @@
 """
 Model library for behavior datasets.
 
-This module provides a collection of machine learning models for behavioral analysis.
+This module provides machine learning models for behavioral analysis.
 
 Usage
 -----
->>> from behavior import model_library
->>>
->>> # Use models by class
->>> model = model_library.behavior_xgboost.BehaviorXGBoostModel(params={...})
->>> model.bind_dataset(dataset)
->>> model.configure(config, run_root)
->>> metrics = model.train()
->>>
->>> # Access helpers
->>> from mosaic.behavior.model_library.helpers import XGB_PARAM_PRESETS
+>>> from mosaic.behavior.model_library.trex_identity_network import TRexIdentityNetwork
 """
-
-# Import shared helpers
-from . import helpers
-
-# Import all models
-from . import behavior_xgboost
-from .behavior_xgboost import BehaviorXGBoostModel
 
 # T-Rex-compatible V200 CNN identity classifier (requires PyTorch)
 from . import trex_identity_network
@@ -32,18 +16,9 @@ from .trex_identity_network import TRexIdentityNetwork
 from . import trex_native_identity
 from .trex_native_identity import TRexNativeIdentityNetwork
 
-# FERAL video behavior classifier -- DEPRECATED, use FeralFeature instead
-from . import feral_model
-from .feral_model import FeralModel
-
 __all__ = [
-    "helpers",
-    "behavior_xgboost",
-    "BehaviorXGBoostModel",
     "trex_identity_network",
     "TRexIdentityNetwork",
     "trex_native_identity",
     "TRexNativeIdentityNetwork",
-    "feral_model",
-    "FeralModel",
 ]
